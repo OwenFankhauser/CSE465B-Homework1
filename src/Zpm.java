b930d30f3d66a7136daeeb818ed92d474bab3290
OwenFankhauser <fankhaon@miamioh.edu>
2024-09-05 20:47:50 -0400

    Validates Z+- files.

--- src/Zpm.java
+++ src/Zpm.java
@@ -8,14 +8,63 @@
  * ○ (10 pts) Basic structure, integer variables only
  * ○ (10 pts) Basic structure, integer and string variables
  * ○ (20 pts) For loops
- * ○ (5 pts) Detection of runtime errors
+ * ○ (5  pts) Detection of runtime errors
  * 
  */
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Zpm {
 
+    // Stores all Z+- integer variables and their names.
+    public HashMap<String,Integer> numVari = new HashMap<String, Integer>();
+    
+    // Stores all Z+- string variables and their names.
+    public HashMap<String, String> strVari = new HashMap<String, String>();
+    
+    /**
+     * This method validates the Z+- file and handles the exception if a 
+     *  non.zpm file is attempted to process. 
+     * 
+     * @param fileName The name of the file entered.
+     * @return in The Scanner object produced if the file is successfully 
+     *  validated.
+     */
+    public static Scanner readFile(String fileName) {
+        try {
+            File zpmFile = new File(fileName);
+            Scanner in = new Scanner(zpmFile);
+            return in;
+        } catch (Exception e) { // Incorrect file
+            System.out.println("Please use a Z+- file with the propper '.zpm' extension.");
+            System.exit(0);
+        }
+        return null; // TODO clean this 
+    }
+    
+    /**
+     * This method does the majority of the interpreting. It will parse the
+     *  
+     *   lines of the file and distribute appropriately for processing.
+     * @param in The Scanner object scanning the file
+     */
+    public static void interpret(Scanner in) {
+        System.out.println("Success!");
+    }
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        // Validate file
+        if (!args[0].contains(".zpm")){
+            System.out.println("Please use a Z+- file with the propper '.zpm' extension.");
+            System.exit(0);
+        }
+        Scanner in = readFile(args[0]);
+        // Interpret file
+        interpret(in);
+        // Close Scanner and end task
+        in.close();
     }
 
 }
