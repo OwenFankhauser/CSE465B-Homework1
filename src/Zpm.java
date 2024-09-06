d6277038b72b490f1f1e9ca939dd9e492d6c0ee7
OwenFankhauser <fankhaon@miamioh.edu>
2024-09-05 20:06:23 -0400

    Project start.

--- src/Zpm.java
+++ src/Zpm.java
@@ -1,4 +1,16 @@
-
+/*
+ * Copyright Owen Fankhauser September 2024
+ * 
+ * CSE 465B
+ * Professor Amjad
+ * 
+ * ● (45 pts) Write an interpreter in Java to execute Z+- programs
+ * ○ (10 pts) Basic structure, integer variables only
+ * ○ (10 pts) Basic structure, integer and string variables
+ * ○ (20 pts) For loops
+ * ○ (5 pts) Detection of runtime errors
+ * 
+ */
 public class Zpm {
 
     public static void main(String[] args) {
